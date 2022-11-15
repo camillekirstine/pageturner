ee1bd056b04b835c3e0ae799d8484fe39e13fc20
Charlie <camille@camillekirstine.com>
2022-11-15 13:18:26 +0100

    added navbar

--- index.php
+++ index.php
@@ -1,8 +1,58 @@
 <?php
 require "settings/init.php";
 
-$blogs = $db->sql("SELECT * FROM blogs");
+$books = $db->sql("SELECT * FROM books");
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
+foreach ($books as $book){
+    echo $book->bookName . "<br>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="da">
+
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Pageturner</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- settings/init.php
+++ settings/init.php
@@ -4,10 +4,10 @@ require "classes/classDB.php";
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost:8888";
-    $DB_NAME = "phpproject";
+    $DB_SERVER = "localhost:8889";
+    $DB_NAME = "books";
     $DB_USER = "root";
-    $DB_PASS = "";
+    $DB_PASS = "root";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
